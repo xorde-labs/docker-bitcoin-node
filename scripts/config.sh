95a941c50216851534956e75f5769f38de2a5d35
XT <xt@xorde.co>
2022-05-26 16:03:33 -0400

    Added automatic wallet creation

--- scripts/config.sh
+++ scripts/config.sh
@@ -22,14 +22,14 @@ fi
 ### Enable wallet:
 if printf "${WALLET_ENABLE}" | grep -q "[Yy1]"; then
   export WALLET_NAME=default_wallet
-  echo "Enabling wallet (${WALLET_NAME})"
-  bitcoin-wallet -wallet=${WALLET_NAME} create
+  echo "Enabling wallet..."
+  bitcoin-wallet -wallet=${WALLET_NAME} create && echo "Wallet [${WALLET_NAME}] created"
   printf "wallet=${WALLET_NAME}\n" >> "${CONFIG_FILE}"
 fi
 
 ### Enable txindex:
 if printf "${TXINDEX_ENABLE}" | grep -q "[Yy1]"; then
-  echo "Enabling txindex"
+  echo "Enabling txindex..."
   printf "txindex=1\n" >> "${CONFIG_FILE}"
 fi
 
@@ -41,7 +41,7 @@ fi
 
 ### Switch to network:
 if [ -n "${NETWORK+1}" ]; then
-  echo "Enabling testnet"
+  echo "Configuring network..."
   case "${NETWORK}" in
     mainnet)
       echo "Network is mainnet"
@@ -72,7 +72,7 @@ fi
 
 ### Setting up RPC server:
 if printf "${RPC_ENABLE}" | grep -q "[Yy1]"; then
-  echo "Enabling RPC server"
+  echo "Enabling RPC server..."
   RPC_PASSWORD=${RPC_PASSWORD:-$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)}
   echo "Using password ${RPC_PASSWORD}"
   ENCRYPTED_PASSWORD=$(./rpcpasswd.py ${RPC_PASSWORD:-})
@@ -82,8 +82,6 @@ if printf "${RPC_ENABLE}" | grep -q "[Yy1]"; then
 	printf "rpcallowip=${RPC_ALLOW:-0.0.0.0/0}\n" >> "${CONFIG_FILE}"
 	printf "rpcauth=${RPC_AUTH}\n" >> "${CONFIG_FILE}"
 	printf "\n[${NETWORK_SECTION:-main}]\nrpcport=8332\nrpcbind=${RPC_BIND:-0.0.0.0}" >> "${CONFIG_FILE}"
-
-	echo "RPC Auth: ${RPC_AUTH}"
 fi
 
 echo "Config initialized completed successfully (${CONFIG_FILE})"
